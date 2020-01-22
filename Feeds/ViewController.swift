eca281dff1e8434b62b3abf2b0b8cd2c065dd57b
sudofluff <sudofluff@icloud.com>
2020-01-22 14:16:22 -0800

    Init

---
+++ Feeds/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Feeds
+//
+//  Created by sudofluff on 1/22/20.
+//  Copyright © 2020 sudofluff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = .red
+  }
+
+
+}
+
